94220d4f6d08da46766308e1d4366a4e17490027
Mzr96 <Maziarmoslemi@gmail.com>
2025-05-30 17:15:53 +0330

    Fix repo name

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/your-repo-name/", // Replace with your repo name
+  base: "/ielts-exam/", // Replace with your repo name
   plugins: [react()],
 });
